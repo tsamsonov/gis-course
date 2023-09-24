fcc313826e8f5a267a79451ac670e81e72d19e16
Timofey Samsonov <tsamsonov@geogr.msu.ru>
2023-09-24 17:09:39 +0300

    add import

--- sql/ch01-5A_AddGeomsql.sql
+++ sql/ch01-5A_AddGeomsql.sql
@@ -1,4 +1,4 @@
 ALTER TABLE postgis.geo_points 
-	ADD IF NOT EXISTS geom geometry(point, 32637);
+  ADD geom geometry(point, 32637);
 UPDATE postgis.geo_points
-	SET geom = ST_Point(x, y)
+  SET geom = ST_Point(x, y)
